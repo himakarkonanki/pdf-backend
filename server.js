5c49be04b5e2ec15d18ebbc8292d56631a8a504a
himakarkonanki <konankihimakar699@gmail.com>
2025-09-11 11:39:51 +0530

    login

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://himakarkonanki.github.io'
+  ],
   credentials: true
 }));
 app.use(bodyParser.json({ limit: '250mb' }));
